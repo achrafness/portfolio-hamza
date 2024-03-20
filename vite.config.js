b2438022161e11434560ea4d1f8eedf065fbb18a
achrafness <achraf.nessighaoui13@gmail.com>
2024-03-20 01:21:38 +0100

    package for github page

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// change the build dircory 
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Change 'new_build_directory_name' to your desired name
+  },
+});
